4c4ea03d545b2f6d507f4a7a7474971291b1653d
meng.jia3 <meng.jia3@ubisoft.com>
2024-06-18 12:28:19 -0400

    feat: setup eventGridTrigger and timerTrigger in azure function

--- my-azure-function/src/functions/eventGridTrigger.js
+++
@@ -1,7 +0,0 @@
-const { app } = require("@azure/functions");
-
-app.eventGrid("eventGridTrigger", {
-  handler: (event, context) => {
-    context.log("Event grid function processed event:", event);
-  },
-});
